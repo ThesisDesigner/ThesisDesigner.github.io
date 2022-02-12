04559e5a75fd61cfcae94f37fa0b6795a0341d89
ThesisDesigner <98523704+ThesisDesigner@users.noreply.github.com>
2022-02-12 23:15:00 +0100

    Update resultController.js

--- controller/resultController.js
+++ controller/resultController.js
@@ -6,19 +6,19 @@
 			document.getElementById("lblquestion").innerHTML = sessionStorage.getItem("questions");
 		}
 		if(sessionStorage.getItem("questions") == "" || sessionStorage.getItem("questions") == "undefined" || sessionStorage.getItem("questions") == null){
-			document.getElementById("lblquestion").innerHTML = "Die Forschungsfrage ist noch nicht definiert.";
+			document.getElementById("lblquestion").innerHTML = "The research question is not defined.";
 		}
 		if(sessionStorage.getItem("design")){
 			document.getElementById("lbldesign").innerHTML = sessionStorage.getItem("design");
 		}
 		if(sessionStorage.getItem("design") == "" || sessionStorage.getItem("design") == "undefined" || sessionStorage.getItem("design") == null){
-			document.getElementById("lbldesign").innerHTML = "Das Forschungsdesign ist noch nicht definiert.";
+			document.getElementById("lbldesign").innerHTML = "The research design is not defined.";
 		}
 		if(sessionStorage.getItem("researchgoal")){
 			document.getElementById("lblgoal").innerHTML = sessionStorage.getItem("researchgoal");
 		}
 		if(sessionStorage.getItem("researchgoal") == "" || sessionStorage.getItem("researchgoal") == "undefined" || sessionStorage.getItem("researchgoal") == null){
-			document.getElementById("lblgoal").innerHTML = "Die Forschungsfrage ist noch nicht definiert.";
+			document.getElementById("lblgoal").innerHTML = "The research goal is not defined.";
 		}
 	 }
 
